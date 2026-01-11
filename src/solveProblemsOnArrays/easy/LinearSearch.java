00e6bdbd023a8751014b41d8ad9bed36254e0fe6
shrsu <sujshrest@gmail.com>
2026-01-11 22:12:02 +0530

    LinearSearch

---
+++ src/solveProblemsOnArrays/easy/LinearSearch.java
@@ -0,0 +1,12 @@
+package solveProblemsOnArrays.easy;
+
+class LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i; // target found
+            }
+        }
+        return -1; // target not found
+    }
+}
