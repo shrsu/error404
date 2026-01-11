9f9e3e160ac9e863f34f596a21843b1c7cf6bbb8
shrsu <sujshrest@gmail.com>
2026-01-11 22:36:01 +0530

    SingleNumber

---
+++ src/solveProblemsOnArrays/easy/SingleNumber.java
@@ -0,0 +1,11 @@
+package solveProblemsOnArrays.easy;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for (int num : nums) {
+            xor ^= num;
+        }
+        return xor;
+    }
+}
