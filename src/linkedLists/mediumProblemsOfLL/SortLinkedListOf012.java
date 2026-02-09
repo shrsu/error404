c4c357b2d2285037e75d5e1c6ec0a9af4b3d2b2a
shrsu <sujshrest@gmail.com>
2026-02-09 22:20:02 +0530

    SortLinkedListOf012

--- src/linkedLists/mediumProblemsOfLL/SortLinkedListOf012.java
+++ src/linkedLists/mediumProblemsOfLL/SortLinkedListOf012.java
@@ -1,4 +1,54 @@
 package linkedLists.mediumProblemsOfLL;
 
 public class SortLinkedListOf012 {
+
+    // Definition of the Node
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Function to sort the linked list by rearranging links
+    static Node sortZeroOneTwo(Node head) {
+
+        // Dummy heads for 0s, 1s, and 2s lists
+        Node zeroDummy = new Node(-1);
+        Node oneDummy = new Node(-1);
+        Node twoDummy = new Node(-1);
+
+        // Tail pointers for the three lists
+        Node zeroTail = zeroDummy;
+        Node oneTail = oneDummy;
+        Node twoTail = twoDummy;
+
+        Node curr = head;
+
+        // Traverse the original list
+        while (curr != null) {
+            if (curr.data == 0) {
+                zeroTail.next = curr;
+                zeroTail = zeroTail.next;
+            } else if (curr.data == 1) {
+                oneTail.next = curr;
+                oneTail = oneTail.next;
+            } else {
+                twoTail.next = curr;
+                twoTail = twoTail.next;
+            }
+            curr = curr.next;
+        }
+
+        // Connect the three lists
+        zeroTail.next = (oneDummy.next != null) ? oneDummy.next : twoDummy.next;
+        oneTail.next = twoDummy.next;
+        twoTail.next = null;
+
+        // New head is the start of the 0s list
+        return zeroDummy.next;
+    }
 }
